9dcc4fee4dd8a18bc7e92b4155912794c84d0323
Jared <jared.i.green@accenture.com>
2019-03-07 15:20:50 -0600

    Add onClickListener to increment counter

--- app/src/androidTest/java/com/example/redesignedcactus/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/redesignedcactus/MainActivityTest.kt
@@ -1,6 +1,7 @@
 package com.example.redesignedcactus
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
@@ -23,6 +24,20 @@ class MainActivityTest {
 
     @Test
     fun counterLabelHasDefaultTextOfZero() {
-        onView(withId(R.id.counterLabel)).check(matches(withText("0")))
+        onView(withId(R.id.progressCounter)).check(matches(withText("0")))
+    }
+
+    @Test
+    fun whenProgressButtonClickedThenCounterLabelIncrementsByOne() {
+        onView(withId(R.id.progressButton)).perform(click())
+        onView(withId(R.id.progressCounter)).check(matches(withText("1")))
+    }
+
+    @Test
+    fun whenProgressButtonClickedMultipleTimesTheCounterLabelIncrementsByOneForEachClick() {
+        onView(withId(R.id.progressButton)).perform(click())
+        onView(withId(R.id.progressButton)).perform(click())
+        onView(withId(R.id.progressButton)).perform(click())
+        onView(withId(R.id.progressCounter)).check(matches(withText("3")))
     }
 }
