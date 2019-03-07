6c6e130c7013792a2ebd6eda0f233f045c83117e
Jared <jared.i.green@accenture.com>
2019-03-07 11:15:09 -0600

    Update to AndroidX and add UI test for MainActivity

---
+++ app/src/androidTest/java/com/example/redesignedcactus/MainActivityTest.kt
@@ -0,0 +1,23 @@
+package com.example.redesignedcactus
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @get:Rule
+    var mainActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun progressButtonHasText() {
+        onView(withId(R.id.progressButton)).check(matches(withText("Increment Progress")))
+    }
+}
